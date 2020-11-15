f8716920372ddcccce74126ef793a17f9046de54
shalitha <shalithax@gmail.com>
2020-11-15 23:15:51 +0530

    chnage default display

--- Server/WebPortal.API/Controllers/WeatherForecastController.cs
+++ Server/WebPortal.API/Controllers/WeatherForecastController.cs
@@ -11,11 +11,7 @@ namespace WebPortal.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
+        
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,16 +20,9 @@ namespace WebPortal.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IActionResult> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok("API SERVER UP");
         }
     }
 }
